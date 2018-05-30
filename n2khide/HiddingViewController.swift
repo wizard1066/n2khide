a4611453900e474ab725a24a556f61262712766d
Mark Lucking <mark.lucking@gmail.com>
2018-05-30 10:48:51 +0200

    Added Split controller, dropzone and tableview

---
+++ n2khide/HiddingViewController.swift
@@ -0,0 +1,71 @@
+//
+//  HiddingViewController.swift
+//  n2khide
+//
+//  Created by localuser on 30.05.18.
+//  Copyright © 2018 cqd.ch. All rights reserved.
+//
+
+import UIKit
+
+class HiddingViewController: UIViewController, UIDropInteractionDelegate {
+    
+    @IBOutlet var dropZone: UIView! {
+        didSet {
+            dropZone.addInteraction(UIDropInteraction(delegate:  self))
+        }
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return  session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    var imageFetcher: ImageFetcher!
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        imageFetcher = ImageFetcher() { (url, image) in
+            DispatchQueue.main.async {
+                self.hideView.backgroundImage = image
+            }
+        }
+        session.loadObjects(ofClass: NSURL.self) { nsurl in
+            if let url = nsurl.first as? URL {
+                self.imageFetcher.fetch(url)
+            }
+        }
+        session.loadObjects(ofClass: UIImage.self) { images in
+            if let image = images.first as? UIImage {
+                self.imageFetcher.backup = image
+            }
+        }
+    }
+    
+     @IBOutlet weak var hideView: HideView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
